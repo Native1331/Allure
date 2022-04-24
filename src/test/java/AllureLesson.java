0fbef1613bb5243e0d322bb7f4a7610026507cec
Kadysheva Natalya <natalya.kadysheva@nedra.com>
2022-04-24 16:23:28 +0300

    New test

---
+++ src/test/java/AllureLesson.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class AllureLesson {
+
+        @Test
+        public void testGithubIssue() {
+
+              SelenideLogger.addListener("allure", new AllureSelenide());
+            open ("https://github.com");
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+            $(".header-search-input").submit();
+
+            $(linkText("eroshenkoam/allure-example1111")).click();
+            $(partialLinkText("Issues")).click();
+            $(withText("#76")).click();
+
+
+        }
+    }
+
+
---
+++ src/test/java/WebSteps.java
@@ -0,0 +1,58 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.commands.TakeScreenshot;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class WebSteps {
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозитория {repo}")
+    public void searchForRepository(String repo) {
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(repo);
+        $(".header-search-input").submit();
+    }
+
+    @Step("Переходим по ссылке репозитория {repo}")
+    public void clickOnRepositoryLink(String repo) {
+
+        $(linkText(repo)).click();
+    }
+
+    @Step("Кликаем на таб Issues")
+    public void openIssueTab() {
+
+        $(partialLinkText("Issues")).click();
+    }
+
+    @Step("Проверяем, что существует Issue с номером {number}")
+    public void shouldSeeIssueWithNumber(int number) {
+
+        $(withText("#" + number)).should(Condition.visible);
+        attachScreenshot();
+    }
+
+        @Attachment(value = "Мой любимый скриншот", type = "image/png", fileExtension = "png")
+        public TakeScreenshot attachScreenshot() {
+            return ((TakeScreenshot)WebDriverRunner.getWebDriver());
+        }
+    }
+
+
+
+
+
+
